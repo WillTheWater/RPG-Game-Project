de6186c3d65d89d1593b0a6e1fbf68bd36746a14
WillTheWater <willthewater@protonmail.com>
2024-08-28 09:31:39 +0200

    Project repaired and updated

--- SDL2Project1/SDL2Project1/main.cpp
+++ SDL2Project1/SDL2Project1/main.cpp
@@ -1,5 +1,6 @@
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 #include<iostream>
 
@@ -16,7 +17,7 @@ int main(int argc, char* args[])
 
 	}
 
-	SDL_Window* window = SDL_CreateWindow("RPG GAME!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320, 240, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("RPG GAME!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 960, SDL_WINDOW_SHOWN);
 	if (window == NULL)
 	{
 		std::cout << "SDL window Error: " << SDL_GetError() << std::endl;
@@ -32,6 +33,15 @@ int main(int argc, char* args[])
 		system("pause");
 		return 1;
 	}
+	//Fixing aspec ratio
+	SDL_RenderSetLogicalSize(renderer, 320, 240);
+	// Loading an image
+	SDL_Texture* testImage = IMG_LoadTexture(renderer, "assets/girlIdle.png");
+	if (testImage == NULL) { std::cout << "image failed to load\n"; }
+
+
+
+
 
 	bool keepLooping = true;
 	SDL_Event event;
@@ -46,9 +56,10 @@ int main(int argc, char* args[])
 			}
 		}
 
-		SDL_SetRenderDrawColor(renderer, 21, 209, 249, 255);//RGB (e.g R = 0-255)
+		SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);//RGB (e.g R = 0-255)
 		//clear entire screen with current draw colour
 		SDL_RenderClear(renderer);
+
 
 		SDL_Rect rect;
 		rect.x = 10;
@@ -58,18 +69,28 @@ int main(int argc, char* args[])
 		SDL_SetRenderDrawColor(renderer, 34, 76, 22, 255);
 		//draws filled in rectangle to window using rectangles data
 		SDL_RenderFillRect(renderer, &rect);
+		
+		SDL_Rect srcRect;
+		srcRect.x = 0;
+		srcRect.y = 0;
+		srcRect.w = 107;
+		srcRect.h = 137;
 
+		SDL_Rect destRect;
+		destRect.x = 70;
+		destRect.y = 20;
+		destRect.w = 107;
+		destRect.h = 137;
+		SDL_RenderCopy(renderer, testImage, &srcRect, &destRect);
+		
 		//swaps drawing buffer
 		SDL_RenderPresent(renderer);
 
-		//ticks are milliseconds since the start of SDL init
-		
+
 	}
 	//CLEANUP
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
-	//lets user interact by pressing anykey
 	return 0;
 }
